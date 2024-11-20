ada09bc0155806366264241bc4bd4f3673873e72
Rudraksh Joshi <u22ee054@eed.svnit.ac.in>
2024-11-21 00:47:13 +0530

    pfp viiewing integration

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf('7967859102:AAHtM2wtWPn_8AiIxO01qYj9zC8pZiu4Ciw');
 
 bot.start((ctx) => {
-  ctx.reply(`Hello, ${ctx.from.first_name}! Welcome how may I help you,to see more options,press /welp .`);
+  ctx.reply(`Hello, ${ctx.from.first_name}! Welcome. How may I help you? To see more options, press /welp.`);
 });
 
 bot.help((ctx) => {
-  ctx.reply('You can use the following commands:\n/start - Start the bot\n/welp - Get help\n/echo - Echo back your message.');
+  ctx.reply('You can use the following commands:\n/start - Start the bot\n/welp - Get help\n/echo - Echo back your message\n/profile - Display your profile photo.');
 });
 
 bot.command('echo', (ctx) => {
@@ -16,6 +16,24 @@ bot.command('echo', (ctx) => {
     ctx.reply(`Echo: ${message}`);
   } else {
     ctx.reply('Please provide a message to echo.');
+  }
+});
+
+// New command to fetch and display profile photo
+bot.command('profile', async (ctx) => {
+  try {
+    const userId = ctx.from.id;
+    const photos = await ctx.telegram.getUserProfilePhotos(userId);
+    
+    if (photos.total_count > 0) {
+      const fileId = photos.photos[0][0].file_id;
+      await ctx.replyWithPhoto(fileId, { caption: `Here is your profile photo, ${ctx.from.first_name}!` });
+    } else {
+      ctx.reply("You don't have a profile photo set on Telegram.");
+    }
+  } catch (err) {
+    console.error('Error fetching profile photo:', err);
+    ctx.reply('Failed to retrieve your profile photo. Please try again later.');
   }
 });
 
